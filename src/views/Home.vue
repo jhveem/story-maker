5b14a043e2e2834e9650066cab88e6b9c78cab2a
Vulhar <joshua.hveem@gmail.com>
2019-04-17 22:25:50 -0600

    changed home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  	<p>This is a demo for plotting out a story with multiple endings. You do this by creating multiple stories and then linking them as paths. So Story 1 branches out to Story 1a and Story 1b and then those can both branch out and so on.
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
