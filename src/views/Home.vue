6d3a92c42bd61432f26cf194548ed230c945c35a
Vulhar <joshua.hveem@gmail.com>
2019-04-17 22:27:52 -0600

    minor edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-  	<p>This is a demo for plotting out a story with multiple endings. You do this by creating multiple stories and then linking them as paths. So Story 1 branches out to Story 1a and Story 1b and then those can both branch out and so on.
+  	<p>This is a demo for plotting out a story with multiple endings. You do this by creating multiple stories and then linking them as paths. So Story 1 branches out to Story 1a and Story 1b and then those can both branch out and so on.</p>
   </div>
 </template>
 
